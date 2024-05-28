c63d8a4549d6f71d537a1ae43287f596299b8b5b
Shigenobu2 <adachi.shigenobu.so@mynavi.jp>
2024-05-28 07:19:12 +0000

    給与アプリ最低限完成

---
+++ adachi/calcsalary_app/salary/views.py
@@ -0,0 +1,30 @@
+from flask import request, redirect, url_for, render_template, flash, session
+from salary import app
+from decimal import Decimal, ROUND_HALF_UP
+
+# 最初のページ
+@app.route("/")
+def show_salary():
+    return render_template('input.html')
+
+@app.route('/output', methods=['GET', 'POST'])
+def input():
+    income = 0
+    tax = 0
+    # if request.method == 'POST': # 正しく送信
+    suuzi = int(request.form['money'])
+    tax = 0 #　税
+    income = 0 #　手取り
+
+    if suuzi >= 1000000:
+        tax = int((suuzi-1000000) * 0.2) + int(1000000 * 0.1)
+    else:
+        tax = int(suuzi * 0.1)
+
+    tax = Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
+    income = suuzi - tax
+    print("支給額:"+str(income)+"、税額:"+str(tax),end="")
+
+    # flash('計算結果を出力')
+        # return redirect(url_for('show_salary'))
+    return render_template('output.html',suuzi=suuzi,income=income,tax=tax)
