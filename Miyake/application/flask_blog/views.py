e984e309aaa3704214e8f03077568451a4268cc4
MokaMiyake <miyake.moka.dr@mynavi.jp>
2024-05-28 08:34:25 +0000

    給与計算アプリの作成

--- Miyake/application/flask_blog/views.py
+++ Miyake/application/flask_blog/views.py
@@ -4,8 +4,8 @@ from flask_blog import app
 @app.route('/')
 def show_entries():
     if not session.get('logged_in'):
-        #return redirect('/login') #ログインしていない場合には、ログイン画面に自動で遷移する
-        return redirect(url_for('login')) #自動でリンクを作成（ｐ114）
+        return redirect('/login') #ログインしていない場合には、ログイン画面に自動で遷移する
+        #return redirect(url_for('login')) #自動でリンクを作成（ｐ114）
     return render_template('entries/index.html')                     
 
 @app.route('/login',methods=['GET','POST'])
@@ -20,13 +20,13 @@ def login():
         else:
             session['logged_in'] = True #変数session中のlogged_inの値をTrueにセット　→　この値をリクエストのたびチェックすることでログインしているか判別する
             flash('ログインしました')
-            #return redirect('/')
-            return redirect(url_for('show_entries'))
+            return redirect('/')
+            #return redirect(url_for('show_entries'))
     return render_template('login.html')
     
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None) #ログアウトしたら、session情報を削除する（18行目session['logged_in'] = Trueと比較？）
     flash('ログアウトしました')
-    #return redirect('/')
-    return redirect(url_for('show_entries'))
+    return redirect('/')
+    #return redirect(url_for('show_entries'))
---
+++ Miyake/calcsalary_app/server.py
@@ -0,0 +1,4 @@
+from salary import app
+
+if __name__ == '__main__': #このファイルが直接実行されたとき実行される処理（=python3 server.py)
+    app.run()
