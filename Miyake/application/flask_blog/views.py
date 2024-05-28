44fd3a8ce755dc2dc4955a6e2971b3c05ece2a5b
MokaMiyake <miyake.moka.dr@mynavi.jp>
2024-05-28 05:54:58 +0000

    url_forの追加

--- Miyake/application/flask_blog/views.py
+++ Miyake/application/flask_blog/views.py
@@ -3,19 +3,30 @@ from flask_blog import app
 
 @app.route('/')
 def show_entries():
-    return render_template('entries/index.html')
+    if not session.get('logged_in'):
+        #return redirect('/login') #ログインしていない場合には、ログイン画面に自動で遷移する
+        return redirect(url_for('login')) #自動でリンクを作成（ｐ114）
+    return render_template('entries/index.html')                     
 
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
-            print('ユーザ名が異なります')
+            #print('ユーザ名が異なります') #ターミナルへの表示（webページには表示されない）
+            flash('ユーザ名が異なります') #webページへの表示、ユーザにメッセージが表示される
         elif request.form['password'] != app.config['PASSWORD']:
-            print('パスワードが異なります')
+            #print('パスワードが異なります')
+            flash('パスワードが異なります')
         else:
-            return redirect('/')
+            session['logged_in'] = True #変数session中のlogged_inの値をTrueにセット　→　この値をリクエストのたびチェックすることでログインしているか判別する
+            flash('ログインしました')
+            #return redirect('/')
+            return redirect(url_for('show_entries'))
     return render_template('login.html')
     
 @app.route('/logout')
 def logout():
-    return redirect('/')
+    session.pop('logged_in', None) #ログアウトしたら、session情報を削除する（18行目session['logged_in'] = Trueと比較？）
+    flash('ログアウトしました')
+    #return redirect('/')
+    return redirect(url_for('show_entries'))
