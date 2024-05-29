49b3c9f862dcb6f1551b4c406f8c26d33add3814
MokaMiyake <miyake.moka.dr@mynavi.jp>
2024-05-29 08:28:34 +0000

    給与計算アプリの更新

--- Miyake/calcsalary_app/salary/vies.py
+++ Miyake/calcsalary_app/salary/vies.py
@@ -1,6 +1,12 @@
+#views:ページの動きを設定する　ページ遷移の指定、計算とか
 from flask import request,redirect,url_for,render_template,flash,session
 from salary import app
 
 @app.route('/')
-def show_entries():
-    return render_template('layout.html')
+def show_salary():
+    return render_template('input.html')
+
+@app.route('/output',methods=['GET','POST'])
+def output():
+    print('outputに遷移')
+    return render_template('output.html')
