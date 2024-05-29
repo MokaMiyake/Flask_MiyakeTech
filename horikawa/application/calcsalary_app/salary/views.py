57ba2abbcee472f4436fad64e9e665c9305011e3
Shoji0423 <Horikawa.shoji.hg@mynavi.jp>
2024-05-29 07:43:21 +0000

    投稿記事の表示・編集・削除機能の実装

--- horikawa/application/calcsalary_app/salary/views.py
+++ horikawa/application/calcsalary_app/salary/views.py
@@ -8,17 +8,21 @@ from salary.calc import calc_salary
 def show_entries():
     return render_template("input.html")
 
-@app.route("/calc", methods=["GET", "POST"])
-def calc():
-    # if request.method == "POST":
-    print("計算実行")
+@app.route("/output", methods=["GET", "POST"])
+def output():
+    session["input_data"] = ""
 
     if not request.form["salary"]:
         flash("給与が未入力です。入力してください。")
+
     elif len(request.form["salary"]) > 10:
         flash("給与には9,999,999,999までが入力可能です。")
+        session["input_data"] = request.form["salary"]
+
     elif request.form["salary"][0] == "-":
         flash("給与にはマイナスの値は入力できません")
+        session["input_data"] = request.form["salary"]
+
     else:
         input_salary = request.form["salary"]
 
