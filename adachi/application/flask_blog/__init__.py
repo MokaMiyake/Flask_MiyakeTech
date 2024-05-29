ac554fb48133334cd861ff926eedefe3dd968ee7
Shigenobu2 <adachi.shigenobu.so@mynavi.jp>
2024-05-29 09:04:54 +0000

    fla_29CRUD

--- adachi/application/flask_blog/__init__.py
+++ adachi/application/flask_blog/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-import flask_blog.views
-
 app.config.from_object("flask_blog.config")
 
+db = SQLAlchemy(app)
+
+from flask_blog.views import views, entries
 
 
