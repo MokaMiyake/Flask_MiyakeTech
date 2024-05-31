c72cd99e7e51d521682131e1107338eb41016d59
Shoji0423 <Horikawa.shoji.hg@mynavi.jp>
2024-05-31 04:48:38 +0000

    基本的な画面の動きを実装。後はデータのやり取りの処理を実装予定

--- horikawa/application/holiday_app/holiday/models/mst_holiday.py
+++ horikawa/application/holiday_app/holiday/models/mst_holiday.py
@@ -1,9 +1,11 @@
 # 祝日マスタを定義
 
-from holiday_app import db
+from holiday import db
 from datetime import datetime
 
 class Holiday(db.Model):
+    holi_date = db.Column(db.DateTime, primary_key=True)
+    holi_text = db.Column(db.Text)
 
     def __init__(self, holi_date=None, holi_text=None):
         self.holi_date = holi_date
--- horikawa/application/holiday_app/holiday/views/views.py
+++ horikawa/application/holiday_app/holiday/views/views.py
@@ -1,63 +1,24 @@
 from flask import request, redirect, url_for, render_template, flash, session
 from holiday import app
 from decimal import Decimal, ROUND_HALF_UP
+from holiday.models.mst_holiday import Holiday
+from holiday import db
 
 # 最初のページ
 @app.route("/")
-def show_holiday():
+def show_input():
     return render_template('input.html')
 
 @app.route('/result', methods=['GET', 'POST'])
-def input():
-    income = 0
-    tax = 0
-    suuzi = 0
-    
-    if request.method == 'POST': # 正しく送信
-        if validate(request.form['money']) == 0:
-            return redirect(url_for('show_salary'))
-        else:
-            suuzi = int(request.form['money'])
-            tax = calc(suuzi)
-            income = suuzi - tax
-            flash('計算結果を出力')
-        # return redirect(url_for('show_salary'))
-    return render_template('output.html',suuzi=suuzi,income=income,tax=tax)
+def result():
+    if request.form["button"] == "insert_update":
+        flash("新規登録・更新が実行されました")
+    elif request.form["button"] == "delete":
+        flash("削除が実行されました")
+    return redirect(url_for("show_input"))
 
-
-
-# 税金の計算
-def calc(suuzi):
-    tax = 0 #　税
-    income = 0 #　手取り
-
-    if suuzi >= 1000000:
-        tax = int((suuzi-1000000) * 0.2) + int(1000000 * 0.1)
-    else:
-        tax = int(suuzi * 0.1)
-
-    tax = Decimal(str(tax)).quantize(Decimal("0"),rounding=ROUND_HALF_UP)
-    income = suuzi - tax
-
-    # ターミナル上での確認用
-    print("支給額:"+str(income)+"、税額:"+str(tax),end="")
-
-    return tax
-
-def validate(num):
-        '''
-        - 未入力 禁止
-        - 11桁以上 禁止
-        - 「-」 禁止
-        '''
-        if num == '':
-            flash('値を入力してください。')
-            return 0
-        
-        if len(num) > 10:
-            flash('給与には最大9,999,999,999まで入力可能です。')
-            return 0
-        
-        if '-' in num:
-            flash('給与にはマイナスの値は入力できません。')
-            return 0
+# 一覧表示
+@app.route('/list', methods=['POST'])
+def list_view():
+    entries = Holiday.query.order_by(Holiday.holi_date).all()
+    return render_template("list.html",entries=entries)
